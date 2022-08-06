e7a2b5f9292cfd4d5877da9817bb6d057d77ef61
stevencr-123 <110734649+stevencr-123@users.noreply.github.com>
2022-08-06 10:39:28 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.EightG, function () {
+    basic.showNumber(5)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showNumber(4)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showNumber(1)
+})
+input.onGesture(Gesture.SixG, function () {
+    basic.showNumber(7)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showString("2")
+})
+input.onGesture(Gesture.ThreeG, function () {
+    basic.showNumber(6)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showNumber(3)
+})
